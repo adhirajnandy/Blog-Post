51fe416e4fc57bc5ae8d18b6e52b1fbd85fc1d6c
Adhiraj Nandy <adhirajnandy@Adhirajs-MacBook-Air.local>
2025-02-21 18:49:23 +0530

    Error Handlers

--- Company/__init__.py
+++ Company/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from Company.core.views import core
+from Company.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
