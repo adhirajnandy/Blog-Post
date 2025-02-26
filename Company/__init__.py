df056f707740f8ec861b79b33397f882ae6082a6
Adhiraj Nandy <adhirajnandy@Adhirajs-MacBook-Air.local>
2025-02-26 16:40:37 +0530

    Final Commit

--- Company/__init__.py
+++ Company/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 
+
 app = Flask(__name__)
 
 ##### DATABASE SETUP ##########
@@ -23,7 +24,9 @@ login_manager.login_view = 'users.login'
 from Company.core.views import core
 from Company.error_pages.handlers import error_pages
 from Company.users.views import users
+from Company.blog_posts.views import blog_posts
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
